f64155b9dec9bb0b8136922b20622c8ed0eb5b4e
arcticdreamz <tomcrasset@gmail.com>
2018-12-01 23:48:52 +0100

    Sort new merged csv in ascending order

--- joincsv.py
+++ joincsv.py
@@ -5,11 +5,16 @@ user = pd.read_csv('Data/data_user.csv',dtype=object)
 movie = pd.read_csv('Data/data_movie_modified.csv',dtype=object)
 test = pd.read_csv('Data/data_test.csv',dtype=object)
 
-train_user_merge = train.merge(user, on="user_id", how="outer").fillna("")
-train_user_movie_merge = train_user_merge.merge(movie, on="movie_id", how="outer").fillna("")
-train_user_movie_merge.to_csv("Data/train_user_movie_merge.csv", index=False)
+train_movie = pd.merge_ordered(train, movie, left_by="movie_id", how="outer").fillna("")
+train_user_movie = pd.merge_ordered(train_movie, user, left_by="user_id", how="outer").fillna("")
+train_user_movie[["user_id","movie_id"]] = train_user_movie[["user_id","movie_id"]].apply(pd.to_numeric)
+train_user_movie = train_user_movie.sort_values(["user_id","movie_id"],axis=0)
+train_user_movie.to_csv("Data/train_user_movie_merge.csv", index=False)
 
-test_user_merge = test.merge(user, on="user_id", how="outer").fillna("")
-test_user_movie_merge = test_user_merge.merge(movie, on="movie_id", how="outer").fillna("")
-test_user_movie_merge.to_csv("Data/test_user_movie_merge.csv", index=False)
+
+test_movie = pd.merge_ordered(test, movie, left_by="movie_id", how="outer").fillna("")
+test_user_movie = pd.merge_ordered(test_movie, user, left_by="user_id", how="outer").fillna("")
+test_user_movie[["user_id","movie_id"]] = test_user_movie[["user_id","movie_id"]].apply(pd.to_numeric)
+test_user_movie = test_user_movie.sort_values(["user_id","movie_id"],axis=0)
+test_user_movie.to_csv("Data/test_user_movie_merge.csv", index=False)
 
