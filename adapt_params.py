b911b804c85fc022d36a8cddf6309b19ce88e925
arcticdreamz <tomcrasset@gmail.com>
2018-12-13 15:33:13 +0100

    Add figures to a new folder

--- adapt_params.py
+++ adapt_params.py
@@ -13,162 +13,12 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestRegressor
-from sklear.neural_network import MLPRegressor
 # from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error
 
 from sklearn.model_selection import train_test_split
 from sklearn.externals import joblib
 from matplotlib import pyplot as plt
-@contextmanager
-def measure_time(label):
-    """
-    Context manager to measure time of computation.
-    >>> with measure_time('Heavy computation'):
-    >>>     do_heavy_computation()
-    'Duration of [Heavy computation]: 0:04:07.765971'
-
-    Parameters
-    ----------
-    label: str
-        The label by which the computation will be referred
-    """
-    start = time.time()
-    yield
-    end = time.time()
-    print('Duration of [{}]: {}'.format(label,
-                                        datetime.timedelta(seconds=end-start)))
-
-
-def load_from_csv(path, delimiter=','):
-    """
-    Load csv file and return a NumPy array of its data
-
-    Parameters
-    ----------
-    path: str
-        The path to the csv file to load
-    delimiter: str (default: ',')
-        The csv field delimiter
-
-    Return
-    ------
-    D: array
-        The NumPy array of the data contained in the file
-    """
-    return pd.read_csv(path, delimiter=delimiter).values.squeeze()
-
-
-def build_rating_matrix(user_movie_rating_triplets):
-    """
-    Create the rating matrix from triplets of user and movie and ratings.
-
-    A rating matrix `R` is such that `R[u, m]` is the rating given by user `u`
-    for movie `m`. If no such rating exists, `R[u, m] = 0`.
-
-    Parameters
-    ----------
-    user_movie_rating_triplets: array [n_triplets, 3]
-        an array of trpilets: the user id, the movie id, and the corresponding
-        rating.
-        if `u, m, r = user_movie_rating_triplets[i]` then `R[u, m] = r`
-
-    Return
-    ------
-    R: sparse csr matrix [n_users, n_movies]
-        The rating matrix
-    """
-    rows = user_movie_rating_triplets[:, 0]
-    cols = user_movie_rating_triplets[:, 1]
-    training_ratings = user_movie_rating_triplets[:, 2]
-
-    return sparse.coo_matrix((training_ratings, (rows, cols))).tocsr()
-
-
-def create_learning_matrices(rating_matrix, user_movie_pairs):
-    """
-    Create the learning matrix `X` from the `rating_matrix`.
-
-    If `u, m = user_movie_pairs[i]`, then X[i] is the feature vector
-    corresponding to user `u` and movie `m`. The feature vector is composed
-    of `n_users + n_movies` features. The `n_users` first features is the
-    `u-th` row of the `rating_matrix`. The `n_movies` last features is the
-    `m-th` columns of the `rating_matrix`
-
-    In other words, the feature vector for a pair (user, movie) is the
-    concatenation of the rating the given user made for all the movies and
-    the rating the given movie receive from all the user.
-
-    Parameters
-    ----------
-    rating_matrix: sparse matrix [n_users, n_movies]
-        The rating matrix. i.e. `rating_matrix[u, m]` is the rating given
-        by the user `u` for the movie `m`. If the user did not give a rating for
-        that movie, `rating_matrix[u, m] = 0`
-    user_movie_pairs: array [n_predictions, 2]
-        If `u, m = user_movie_pairs[i]`, the i-th raw of the learning matrix
-        must relate to user `u` and movie `m`
-
-    Return
-    ------
-    X: sparse array [n_predictions, n_users + n_movies]
-        The learning matrix in csr sparse format
-    """
-    # Feature for users
-    rating_matrix = rating_matrix.tocsr()
-    user_features = rating_matrix[user_movie_pairs[:, 0]]
-
-
-    rating_matrix = rating_matrix.tocsc()
-    movie_features = rating_matrix[:, user_movie_pairs[:, 1]].transpose()
-    X = sparse.hstack((user_features, movie_features))
-    return X.tocsr()
-
-
-def make_submission(y_predict, user_movie_ids, file_name='submission',
-                    date=True):
-    """
-    Write a submission file for the Kaggle platform
-
-    Parameters
-    ----------
-    y_predict: array [n_predictions]
-        The predictions to write in the file. `y_predict[i]` refer to the
-        user `user_ids[i]` and movie `movie_ids[i]`
-    user_movie_ids: array [n_predictions, 2]
-        if `u, m = user_movie_ids[i]` then `y_predict[i]` is the prediction
-        for user `u` and movie `m`
-    file_name: str or None (default: 'submission')
-        The path to the submission file to create (or override). If none is
-        provided, a default one will be used. Also note that the file extension
-        (.txt) will be appended to the file.
-    date: boolean (default: True)
-        Whether to append the date in the file name
-
-    Return
-    ------
-    file_name: path
-        The final path to the submission file
-    """
-
-    # Naming the file
-    if date:
-        file_name = '{}_{}'.format(file_name, time.strftime('%d-%m-%Y_%Hh%M'))
-
-    file_name = '{}.txt'.format(file_name)
-
-    # Writing into the file
-    with open(file_name, 'w') as handle:
-        handle.write('"USER_ID_MOVIE_ID","PREDICTED_RATING"\n')
-        for (user_id, movie_id), prediction in zip(user_movie_ids,
-                                                 y_predict):
-
-            if np.isnan(prediction):
-                raise ValueError('The prediction cannot be NaN')
-            line = '{:d}_{:d},{}\n'.format(user_id, movie_id, prediction)
-            handle.write(line)
-    return file_name
-
 
 def neuralNet():
     prefix = 'Data/'
@@ -250,8 +100,5 @@ def linearRegression():
 if __name__ == '__main__':
    
     #decisiontreemethod() # Kaggle error of 1.27
-    
-    knrmethod() # Kaggle error of 2.56
-    
     #randomforest()
 
