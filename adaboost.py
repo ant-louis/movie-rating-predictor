6d8c88afb256a89c7d9fcab1eabd02c4b7a04dda
arcticdreamz <tomcrasset@gmail.com>
2018-12-13 17:33:16 +0100

    Adapt the adaboost script to the learning matrix with features

--- adaboost.py
+++ adaboost.py
@@ -30,29 +30,37 @@ import base_methods as base
 def adaboost():
     prefix = 'Data/'
 
-    # ------------------------------- Learning ------------------------------- #
-    # Load training data
-    df = pd.read_csv(os.path.join(prefix, 'train_user_movie_merge.csv'), delimiter=',',dtype=float)
-
-    train_features = df.columns
-    training_with_more_features = df.values.squeeze()  
-
+  #-------------------------------------MATRICES WITH FEATURES --------------------------------------------------------------------------
+    training_user_movie_pairs_features = pd.read_csv(os.path.join(prefix,
+                                                           'train_user_movie_merge.csv'))
     training_labels = base.load_from_csv(os.path.join(prefix, 'output_train.csv'))
 
-    X_train, X_test, y_train, y_test = train_test_split(training_with_more_features, training_labels, test_size=0.2, random_state=42)
+    X_ls, X_ts, y_ls, y_ts = train_test_split(training_user_movie_pairs_features, training_labels, test_size=0.2, random_state=42)
+
+    user_movie_rating_triplets_train = np.hstack((X_ls[['user_id','movie_id']].values,
+                                            y_ls.reshape((-1, 1))))
+    user_movie_rating_triplets_test = np.hstack((X_ts[['user_id','movie_id']].values,
+                                            y_ts.reshape((-1, 1))))
+
+    # Build the learning matrixtraining_with_more_features
+    rating_matrix_train = base.build_rating_matrix(user_movie_rating_triplets_train)
+    rating_matrix_test = base.build_rating_matrix(user_movie_rating_triplets_test)
+
+    X_train = base.create_learning_matrices(rating_matrix_train, X_ls)
+    X_test = base.create_learning_matrices(rating_matrix_test, X_ts)
+
+
+    y_train = y_ls
+    y_test = y_ts
 
     # Best estimator after hyperparameter tuning
-    base_model = RandomForestRegressor(bootstrap=True, n_estimators=38, criterion='mse',verbose=0)
+    base_model = DecisionTreeRegressor()
 
     model =  AdaBoostRegressor(base_model)
     print(model)
     with base.measure_time('Training'):
         model.fit(X_train, y_train)
-    feature_importances = pd.DataFrame(model.feature_importances_,
-                                    index = train_features,
-                                    columns=['importance']).sort_values('importance',ascending=False)
 
-    print(feature_importances)
     #Check for overfitting
     y_pred_test = model.predict(X_test)
     y_pred_train = model.predict(X_train)
@@ -70,16 +78,16 @@ def adaboost():
 
     # -----------------------Submission: Running model on provided test_set---------------------------- #
 
-    #Load test data
-    X_test = base.load_from_csv(os.path.join(prefix, 'test_user_movie_merge.csv'))
-    X_test_user_movie_pairs = base.load_from_csv(os.path.join(prefix, 'data_test.csv'))
+    # #Load test data
+    # X_test = base.load_from_csv(os.path.join(prefix, 'test_user_movie_merge.csv'))
+    # X_test_user_movie_pairs = base.load_from_csv(os.path.join(prefix, 'data_test.csv'))
 
-    #Predict
-    print("Predicting...")
-    y_pred = model.predict(X_test)
+    # #Predict
+    # print("Predicting...")
+    # y_pred = model.predict(X_test)
 
-    fname = base.make_submission(y_pred, X_test_user_movie_pairs, 'AdaboostWithRandomForest')
-    print('Submission file "{}" successfully written'.format(fname))
+    # fname = base.make_submission(y_pred, X_test_user_movie_pairs, 'AdaboostWithRandomForest')
+    # print('Submission file "{}" successfully written'.format(fname))
 
 if __name__ == '__main__':
 
