e83d82f4df2fb8971b91f5a35f089546f5687498
arcticdreamz <tomcrasset@gmail.com>
2018-12-16 16:59:10 +0100

    Clean up DT

--- dt.py
+++ dt.py
@@ -29,20 +29,7 @@ def decision_tree():
         model.fit(X_train, y_train)
        
     # -----------------------Submission: Running model on provided test_set---------------------------- #
-    #Load test data
-    test_user_movie_pairs = base.load_from_csv(os.path.join(prefix, 'data_test.csv'))
-
-    # Build the prediction matrix
-    X_ts = base.create_learning_matrices(R.values, test_user_movie_pairs)
-
-    # Predict
-    y_pred = model.predict(X_ts)
-    for i,y in enumerate(y_pred,0):
-        if y_pred[i] > 5.00:
-            y_pred[i] = 5.00
-    
-    fname = base.make_submission(y_pred, test_user_movie_pairs, 'MF_withDecisionTree')
-    print('Submission file "{}" successfully written'.format(fname))
+    base.submit_from_model(model,"MF_withDecisionTree")
 
 if __name__ == "__main__":
     decision_tree()
