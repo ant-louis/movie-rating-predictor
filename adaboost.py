9731498a930c4d699c78ef050fa8e910a6d889f0
arcticdreamz <tomcrasset@gmail.com>
2018-12-17 14:12:25 +0100

    Add tuning to adaboost

--- adaboost.py
+++ adaboost.py
@@ -6,6 +6,20 @@ from sklearn.ensemble import AdaBoostRegressor
 from sklearn.metrics import mean_squared_error
 import base_methods as base
 
+# ------------------------------- Hyperparameters tuning ------------------------------- #
+def tuning():
+    # Setting the range of parameters
+    n_estimators = list(range(1,100,10))
+    loss = ["linear", "square", "exponential"]
+    learning_rate = [2,1,0.5]
+    grid =      {'n_estimators' : n_estimators,
+                'loss':loss,
+                'learning_rate':learning_rate
+                }
+    base_model = DecisionTreeRegressor()
+    model = AdaBoostRegressor(base_model)
+    optimal_parameters = base.hyper_tuning(model, grid)
+    print('Optimal parameters: ', optimal_parameters)
 
 def adaboost():
     prefix = 'Data/'
